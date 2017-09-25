5e01554a2c17a4144d7a062d581847d4708a2d41
mervin <mjyi93@qq.com>
2017-09-25 20:57:14 +0800

    add files

---
+++ myJanDan/Util/TXNavigationTitleView.h
@@ -0,0 +1,13 @@
+//
+//  TXNavigationTitleView.h
+//  myJanDan
+//
+//  Created by mervin on 2017/9/25.
+//  Copyright © 2017年 浅浅浅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TXNavigationTitleView : UIControl
+
+@end
